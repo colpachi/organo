55c39340cd543cc8223704035fa08bff30bba419
Murilo Colpachi <murilobcolpachi@gmail.com>
2022-11-07 10:59:20 -0300

    passagem de parametros por componente via props

--- organo/src/componentes/CampoTexto/index.js
+++ organo/src/componentes/CampoTexto/index.js
@@ -1,11 +1,13 @@
 import './CampoTexto.css'
 
-const CampoTexto = () => {
+const CampoTexto = (props) => {
+
+    const placeholderModificada = `${props.placeholder}...`
 
     return (
         <div className="campo-texto">
-            <label>Nome</label>
-            <input placeholder="Digite o seu nome"/>
+            <label>{props.label}</label>
+            <input placeholder={placeholderModificada}/>
         </div>
     )
 
