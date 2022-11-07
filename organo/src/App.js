6db3d2fee78bd6ece03aabb6c89e8541dc8716f4
Murilo Colpachi <murilobcolpachi@gmail.com>
2022-11-07 10:42:59 -0300

    commit inicial

---
+++ organo/src/App.js
@@ -0,0 +1,13 @@
+import Banner from './componentes/Banner/';
+import CampoTexto from './componentes/CampoTexto';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner/>
+      <CampoTexto/>
+    </div>
+  );
+}
+
+export default App;
---
+++ organo/src/componentes/CampoTexto/index.js
@@ -0,0 +1,14 @@
+import './CampoTexto.css'
+
+const CampoTexto = () => {
+
+    return (
+        <div className="campo-texto">
+            <label>Nome</label>
+            <input placeholder="Digite o seu nome"/>
+        </div>
+    )
+
+}
+
+export default CampoTexto
