4f95876c2641f9a20e62851b70c7e222d47c355a
Murilo Colpachi <murilobcolpachi@gmail.com>
2022-11-10 07:11:45 -0300

    controladores de estado

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -1,6 +1,8 @@
 import './Formulario.css'
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
+import Botao from '../Botao'
+import { useState } from 'react'
 
 const Formulario = () => {
 
@@ -14,14 +16,52 @@ const Formulario = () => {
         'Inovação e Gestão'
     ]
 
+
+    //iniciador e setter de estado
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
+
+    const aoSalvar = (evento) =>{
+        evento.preventDefault()
+        console.log('Form foi submetido => ', nome, cargo, imagem, time);
+    }
+
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome" />
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-                <ListaSuspensa label="Time" itens={times}/>
+                <CampoTexto 
+                    obrigatorio={true}
+                    label="Nome"
+                    placeholder="Digite seu nome"
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}
+                />
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label="Cargo" 
+                    placeholder="Digite seu cargo"
+                    valor={cargo}
+                    aoAlterado={valor =>setCargo(valor)}
+                />
+                <CampoTexto 
+                    label="Imagem" 
+                    placeholder="Digite o endereço da imagem"
+                    valor={imagem}
+                    aoAlterado={valor =>setImagem(valor)}
+                />
+                <ListaSuspensa 
+                    obrigatorio={true} 
+                    label="Time" 
+                    itens={times}
+                    valor={time}
+                    aoAlterado={valor => setTime(valor)}
+                />
+                <Botao>
+                    Criar card
+                </Botao>
             </form>
         </section>
     )
